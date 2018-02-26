8fdef49c2736b36bf5be6bb9d10465df7d223846
Antonia Cronin <antoniajcronin@gmail.com>
2018-02-26 21:00:00 +0000

    Move test/code from Account class

--- jasmine/jasmine-standalone-2.9.1/spec/AccountSpec.js
+++ jasmine/jasmine-standalone-2.9.1/spec/AccountSpec.js
@@ -1,40 +1,35 @@
 describe('Account', function(){
   var account;
-  var transaction_date;
 
-  beforeEach( function(){
-    account = new Account();
-  });
+  describe('initialise', function(){
 
-  it('balance is zero', function(){
-    expect(account.balance).toEqual(0)
-  });
-  it('has a date', function(){
-    expect(account.date).not.toBe(null)
-  });
-  it('balance is £10', function(){
-    account.deposit(10)
-    expect(account.balance).toEqual(10)
+    beforeEach( function(){
+      account = new Account();
+    });
+    it('balance is zero', function(){
+      expect(account.balance).toEqual(0)
+    });;
   });
 
-  it('statement stores deposit balance', function(){
-    account.deposit(5);
-    expect(account.transactions[0]).toEqual(5);
+  describe('deposits', function(){
+
+    beforeEach( function(){
+      account = new Account();
+    });
+    it('balance is £10', function(){
+      account.deposit(10)
+      expect(account.balance).toEqual(10)
+    });
   });
-  it('statement stores deposit as credit', function(){
-    account.deposit(5);
-    expect(account.transactions[2]).toEqual('Credit');
-  });
-  it('balance is -£10', function(){
-    account.withdraw(10)
-    expect(account.balance).toEqual(-10)
-  });
-  it('statement stores withdrawal balance', function(){
-    account.withdraw(5);
-    expect(account.transactions[0]).toEqual(-5);
-  });
-  it('statement stores withdrawal as debit', function(){
-    account.withdraw(5);
-    expect(account.transactions[2]).toEqual('Debit');
+
+  describe('withdrawals', function(){
+
+    beforeEach( function(){
+      account = new Account();
+    });
+    it('balance is -£10', function(){
+      account.withdraw(10)
+      expect(account.balance).toEqual(-10)
+    });
   });
 });
--- jasmine/jasmine-standalone-2.9.1/src/Account.js
+++ jasmine/jasmine-standalone-2.9.1/src/Account.js
@@ -1,23 +1,20 @@
-function Account(){
-  this.balance = 0;
-  var date;
-  this.transactions = [];
-  var state;
-}
+(function(exports){
 
-Account.prototype.deposit = function (amount) {
-  this.balance += amount;
-  state = 'Credit';
-  this.storeTransaction(this.balance, state);
+  function Account(){
+    this.balance = 0;
+    this.isCredit = false;
+    this.transactionClass = new Transaction();
+  }
 
-};
-Account.prototype.withdraw = function (amount) {
-  this.balance -= amount;
-    state = 'Debit';
-  this.storeTransaction(this.balance, state);
-};
+  Account.prototype.deposit = function (amount) {
+    this.balance += amount;
+    this.isCredit = true;
+    this.transactionClass.storeTransaction(amount, this.isCredit, this.balance);
+  };
+  Account.prototype.withdraw = function (amount) {
+    this.balance -= amount;
+    this.transactionClass.storeTransaction(amount, this.isCredit, this.balance);
+  };
 
-Account.prototype.storeTransaction = function (balance, state) {
-  date = new Date();
-  this.transactions.push(balance, date, state);
-};
+  exports.Account = Account;
+})(this)
