eb2a89be09c3811695d93b4bb95e88591343df8a
Antonia Cronin <antoniajcronin@gmail.com>
2018-03-22 18:16:38 +0000

    update Account class

--- jasmine/jasmine-standalone-2.9.1/spec/AccountSpec.js
+++ jasmine/jasmine-standalone-2.9.1/spec/AccountSpec.js
@@ -2,31 +2,23 @@
 describe('Account', function(){
   var account;
 
-  describe('initialise', function(){
+  beforeEach( function(){
+    account = new Account();
+  });
 
-    beforeEach( function(){
-      account = new Account();
-    });
+  describe('initialise', function(){
     it('balance is zero', function(){
       expect(account.balance).toEqual(0)
-    });;
+    });
   });
 
   describe('deposits', function(){
-
-    beforeEach( function(){
-      account = new Account();
-    });
     it('balance is £10', function(){
       expect(account.deposit(10)).toEqual(10)
     });
   });
 
   describe('withdrawals', function(){
-
-    beforeEach( function(){
-      account = new Account();
-    });
     it('balance is -£10', function(){
       expect(account.withdraw(10)).toEqual(-10)
     });
--- jasmine/jasmine-standalone-2.9.1/src/Account.js
+++ jasmine/jasmine-standalone-2.9.1/src/Account.js
@@ -1,15 +1,20 @@
 (function(exports){
-
   function Account(){
     this.balance = 0;
     this.isCredit = false;
+    this.transactionClass = new Transaction();
   }
 
   Account.prototype.deposit = function (amount) {
-    return this.balance += amount;
+    this.balance += amount;
+    this.isCredit = true;
+    this.transactionClass.createTransaction(amount, this.isCredit, this.balance);
+    return this.balance;
   };
   Account.prototype.withdraw = function (amount) {
-    return this.balance -= amount;
+    this.balance -= amount;
+    this.transactionClass.createTransaction(amount, this.isCredit, this.balance);
+    return this.balance;
   };
 
   exports.Account = Account;
